3c18e4805f6d27c261ef26ecbb01a59f07b94098
parkgippeum <106367595+parkgippeum@users.noreply.github.com>
2024-07-24 20:00:25 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+AOS.init({
+  duration: 1300
+});
+
+$(function(){
+  $('.tabcontent > div').hide();
+  $('.tabnav a').click(function () {
+    $('.tabcontent > div').hide().filter(this.hash).fadeIn();
+    $('.tabnav a').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  }).filter(':eq(0)').click();
+  });
